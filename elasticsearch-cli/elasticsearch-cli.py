3588806ec11dacb279f8e554baf21a83ffc4c6a4
teddy <teddy@podotree.com>
2018-05-31 17:25:00 +0900

    cat 요청 추가

--- elasticsearch-cli/elasticsearch-cli.py
+++ elasticsearch-cli/elasticsearch-cli.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import argparse
 import readline
+import json
 
 import requests
 
@@ -9,15 +10,13 @@ from completer import CustomCompleter
 
 es_host = '127.0.0.1'
 es_port = '9200'
-
+request_host = ''
 
 support_commands = ['get', 'set', 'del', 'keys']
 
 
 def check_connection():
     headers = {'Content-Type': 'application/json; charset=utf-8'}
-    es_uri = es_host + ':' + es_port
-    request_host = 'http://' + es_uri if 'http://' not in es_uri else es_uri
     try:
         response = requests.get(request_host, headers=headers)
         print(response.text)
@@ -56,6 +55,27 @@ def command_keys(command: str):
         return
 
 
+def command_cat(command_list: list):
+
+    uri = '/'
+    if len(command_list) > 0:
+        # _cat 으로 통일하기 위해 cat 문구는 모두 삭제 하고 / 로 이어주자
+        command_list.pop(0)
+        uri += '/'.join(command_list)
+
+    try:
+        cat_request_uri = request_host + "/_cat" + uri
+        response = requests.get(cat_request_uri)
+
+        if response.status_code != 200:
+            error_res = json.loads(response.text)
+            print('(error) ' + error_res.get('error').get('reason'))
+            return
+
+        print(response.text)
+    except Exception as e:
+        print(e)
+
 
 def split_command(command: str):
     command_list = command.split(' ')
@@ -77,28 +97,40 @@ if __name__ == '__main__':
     es_host = args.host if args.host else es_host
     es_port = args.port if args.port else es_port
 
+    es_uri = es_host + ':' + es_port
+    request_host = 'http://' + es_uri if 'http://' not in es_uri else es_uri
+
     completer = CustomCompleter(support_commands)
     readline.set_completer(completer.complete)
     readline.parse_and_bind('tab: complete')
 
     check_connection()
-    input_text = es_host.replace('http://', '') + ':' + es_port + '> '
+    input_text = es_uri + '> '
     while True:
         user_input = input(input_text)
         readline.add_history(user_input)
 
-        if user_input == 'exit':
+        command_split_list = user_input.split(' ')
+        command_master = command_split_list[0]
+
+        if command_master == 'exit':
             exit(1)
-        elif user_input == 'help':
+        elif command_master == 'help':
             help_input()
-        elif 'get' in user_input:
+        elif command_master == 'get':
             command_get(user_input)
-        elif 'del' in user_input:
+        elif command_master == 'del':
             command_del(user_input)
-        elif 'set' in user_input:
+        elif command_master == 'set':
             command_set(user_input)
-        elif 'keys' in user_input:
+        elif command_master == 'keys':
             command_keys(user_input)
+        elif command_master == 'cat':
+            command_cat(command_split_list)
+        elif command_master.replace(' ', '') == '':
+            continue
+        else:
+            print("(error) ERR unknown command '" + user_input + "'")
 
 
 
